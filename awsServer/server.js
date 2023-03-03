66250cda634cf5242047ed9b8bd53590ed60b6ec
Dimitriy Glefa <dystopyglefa@gmail.com>
2023-03-03 17:06:18 +0200

    restoration

---
+++ awsServer/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const cors = require('cors');
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    console.log(req)
+})
+app.get('/a', (req, res) => {
+    res.send('you are AWESOME!')
+    console.log(req)
+})
+
+app.get('/b', (req, res) => {
+    res.send('really really AWESOME!!!!!!!!!!!')
+    console.log(req)
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
